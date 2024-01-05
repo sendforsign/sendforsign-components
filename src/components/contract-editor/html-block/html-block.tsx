03a47886b693b29ac8b7fd16f586403ed2d21c76
ilya-bov <111734093+ilya-bov@users.noreply.github.com>
2024-01-05 15:48:18 +0100

    Editor sryles

--- src/components/contract-editor/html-block/html-block.tsx
+++ src/components/contract-editor/html-block/html-block.tsx
@@ -1,218 +1,216 @@
-import React, { FC, useContext, useEffect, useRef, useState } from 'react';
+import React, { FC, useContext, useEffect, useRef, useState } from "react";
 // import './editor.css';
-import 'quill/dist/quill.bubble.css';
-import QuillNamespace, { Quill } from 'quill';
-import QuillBetterTable from 'quill-better-table';
-import { useDebouncedCallback } from 'use-debounce';
-import { Space, Card, Typography } from 'antd';
-import axios from 'axios';
-import dayjs from 'dayjs';
-import utc from 'dayjs/plugin/utc';
-import { useContractEditorContext } from '../contract-editor-context';
-import { BASE_URL } from '../../../config/config';
-import { Action, ApiEntity } from '../../../config/enum';
-import env from 'dotenv';
+import "quill/dist/quill.bubble.css";
+import QuillNamespace, { Quill } from "quill";
+import QuillBetterTable from "quill-better-table";
+import { useDebouncedCallback } from "use-debounce";
+import { Space, Card, Typography } from "antd";
+import axios from "axios";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import { useContractEditorContext } from "../contract-editor-context";
+import { BASE_URL } from "../../../config/config";
+import { Action, ApiEntity } from "../../../config/enum";
+import env from "dotenv";
 //env.config();
 
 QuillNamespace.register(
-	{
-		'modules/better-table': QuillBetterTable,
-	},
-	true
+  {
+    "modules/better-table": QuillBetterTable,
+  },
+  true
 );
 export const HtmlBlock = () => {
-	dayjs.extend(utc);
-	const {
-		contractKey,
-		clientKey,
-		userKey,
-		contractValue,
-		setContractValue,
-		sign,
-		setSign,
-		contractSign,
-		setContractSign,
-		continueLoad,
-	} = useContractEditorContext();
-	const { Title, Text } = Typography;
-	const quillRef = useRef<Quill>();
-	const container = document.querySelector('#contract-editor-container');
-	const [containerInit, setContainerInit] = useState(false);
-	
-	useEffect(() => {
-		if (container && !containerInit) {
-			setContainerInit(true);
-			quillRef.current = new QuillNamespace('#contract-editor-container', {
-				modules: {
-					toolbar: {
-						container: [
-							['bold', 'italic', 'underline', 'strike', 'blockquote'],
-							[{ color: [] }, { background: [] }],
-							[
-								{ list: 'ordered' },
-								{ list: 'bullet' },
-								{ indent: '-1' },
-								{ indent: '+1' },
-							],
-							[{ align: [] }],
-							['link', 'image', 'table'],
-							['blockquote', 'code-block'],
-							[{ direction: 'rtl' }],
-						],
-						handlers: {
-							table: addTable,
-						},
-					},
-					table: false, // disable table module
-					'better-table': {
-						operationMenu: {
-							items: {
-								unmergeCells: {
-									text: 'Unmerge',
-								},
-							},
-						},
-					},
-					keyboard: {
-						bindings: QuillBetterTable.keyboardBindings,
-					},
-					history: {
-						delay: 5000,
-						maxStack: 5000,
-						userOnly: true,
-					},
-				},
-				scrollingContainer: 'body',
-				theme: 'bubble',
-			});
+  dayjs.extend(utc);
+  const {
+    contractKey,
+    clientKey,
+    userKey,
+    contractValue,
+    setContractValue,
+    sign,
+    setSign,
+    contractSign,
+    setContractSign,
+    continueLoad,
+  } = useContractEditorContext();
+  const { Title, Text } = Typography;
+  const quillRef = useRef<Quill>();
+  const container = document.querySelector("#contract-editor-container");
+  const [containerInit, setContainerInit] = useState(false);
 
-			if (quillRef.current) {
-				quillRef.current
-					.getModule('toolbar')
-					.container.addEventListener(
-						'mousedown',
-						(e: {
-							preventDefault: () => void;
-							stopPropagation: () => void;
-						}) => {
-							e.preventDefault();
-							e.stopPropagation();
-						}
-					);
+  useEffect(() => {
+    if (container && !containerInit) {
+      setContainerInit(true);
+      quillRef.current = new QuillNamespace("#contract-editor-container", {
+        modules: {
+          toolbar: {
+            container: [
+              ["bold", "italic", "underline", "strike", "blockquote"],
+              [{ color: [] }, { background: [] }],
+              [
+                { list: "ordered" },
+                { list: "bullet" },
+                { indent: "-1" },
+                { indent: "+1" },
+              ],
+              [{ align: [] }],
+              ["link", "image", "table"],
+              ["blockquote", "code-block"],
+              [{ direction: "rtl" }],
+            ],
+            handlers: {
+              table: addTable,
+            },
+          },
+          table: false, // disable table module
+          "better-table": {
+            operationMenu: {
+              items: {
+                unmergeCells: {
+                  text: "Unmerge",
+                },
+              },
+            },
+          },
+          keyboard: {
+            bindings: QuillBetterTable.keyboardBindings,
+          },
+          history: {
+            delay: 5000,
+            maxStack: 5000,
+            userOnly: true,
+          },
+        },
+        scrollingContainer: "body",
+        placeholder: 'Compose an epic...',
+        theme: "bubble",
+      });
 
-				quillRef.current.on(
-					'text-change',
-					function (delta: any, oldDelta: any, source: any) {
-						if (source === 'user') {
-							handleChangeText(
-								quillRef?.current ? quillRef?.current?.root?.innerHTML : ''
-							);
-						}
-					}
-				);
-			}
-		}
-	}, [container]);
-	useEffect(() => {
-		if (contractValue) {
-			quillRef?.current?.clipboard.dangerouslyPasteHTML(contractValue);
-			setContractValue('');
-		}
-	}, [contractValue]);
-	useEffect(() => {
-		if (sign && contractSign) {
-			let textTmp =
-				quillRef?.current?.root.innerHTML +
-				`<br></br><p><img src='${sign}' alt="signature" /></p>`;
-			textTmp = textTmp + `<p>Name: ${contractSign.fullName}</p>`;
-			textTmp = textTmp + `<p>Email: ${contractSign.email}</p>`;
-			textTmp =
-				textTmp +
-				`<p>Timestamp: ${dayjs(contractSign.createTime).format(
-					'YYYY-MM-DD HH:mm:ss'
-				)} GMT</p>`;
+      if (quillRef.current) {
+        quillRef.current
+          .getModule("toolbar")
+          .container.addEventListener(
+            "mousedown",
+            (e: {
+              preventDefault: () => void;
+              stopPropagation: () => void;
+            }) => {
+              e.preventDefault();
+              e.stopPropagation();
+            }
+          );
 
-			quillRef?.current?.clipboard.dangerouslyPasteHTML(textTmp);
-			handleChangeText(textTmp);
-			setSign('');
-			setContractSign({});
-			quillRef?.current?.enable(false);
-			sendEmail();
-		}
-	}, [sign, contractSign]);
-	const addTable = () => {
-		debugger;
-		(quillRef.current as QuillNamespace)
-			.getModule('better-table')
-			.insertTable(3, 3);
-	};
-	const handleChangeText = useDebouncedCallback(
-		async (content: string) => {
-			let body = {
-				data: {
-					action: Action.UPDATE,
-					clientKey: clientKey,
-					userKey: userKey,
-					contract: { contractKey: contractKey, value: content },
-				},
-			};
-			await axios
-				.post(BASE_URL + ApiEntity.CONTRACT, body, {
-					headers: {
-						Accept: 'application/vnd.api+json',
-						'Content-Type': 'application/vnd.api+json',
-						'x-sendforsign-key': 're_api_key', //process.env.SENDFORSIGN_API_KEY,
-					},
-					responseType: 'json',
-				})
-				.then((payload) => {
-					console.log('editor read', payload);
-				});
-		},
-		5000,
-		// The maximum time func is allowed to be delayed before it's invoked:
-		{ maxWait: 5000 }
-	);
-	const sendEmail = async () => {
-		let body = {
-			clientKey: clientKey,
-			contractKey: contractKey,
-		};
-		await axios
-			.post(BASE_URL + ApiEntity.CONTRACT_EMAIL_SIGN, body, {
-				headers: {
-					Accept: 'application/vnd.api+json',
-					'Content-Type': 'application/vnd.api+json',
-					'x-sendforsign-key': 're_api_key', //process.env.SENDFORSIGN_API_KEY,
-				},
-				responseType: 'json',
-			})
-			.then((payload) => {
-				console.log('editor read', payload);
-			});
-	};
+        quillRef.current.on(
+          "text-change",
+          function (delta: any, oldDelta: any, source: any) {
+            if (source === "user") {
+              handleChangeText(
+                quillRef?.current ? quillRef?.current?.root?.innerHTML : ""
+              );
+            }
+          }
+        );
+      }
+    }
+  }, [container]);
+  useEffect(() => {
+    if (contractValue) {
+      quillRef?.current?.clipboard.dangerouslyPasteHTML(contractValue);
+      setContractValue("");
+    }
+  }, [contractValue]);
+  useEffect(() => {
+    if (sign && contractSign) {
+      let textTmp =
+        quillRef?.current?.root.innerHTML +
+        `<br></br><p><img src='${sign}' alt="signature" /></p>`;
+      textTmp = textTmp + `<p>Name: ${contractSign.fullName}</p>`;
+      textTmp = textTmp + `<p>Email: ${contractSign.email}</p>`;
+      textTmp =
+        textTmp +
+        `<p>Timestamp: ${dayjs(contractSign.createTime).format(
+          "YYYY-MM-DD HH:mm:ss"
+        )} GMT</p>`;
 
-	return (
-		<Card
-			style={{ display: 'flex' }}
-			bordered={true}
-			id='part-2'
-			loading={continueLoad}
-		>
-			<Space direction='vertical' size={16} style={{ display: 'flex' }}>
-				<Space direction='vertical' size={2}>
-					<Title level={4} style={{ margin: '0 0 0 0' }}>
-						Review your document, highlight text to see options
-					</Title>
-					<Text type='secondary'>
-						The green text is where you may want to replace it with your own
-						text.
-					</Text>
-				</Space>
-				<div id='scroll-container'>
-					<div id='contract-editor-container' />
-				</div>
-			</Space>
-		</Card>
-	);
+      quillRef?.current?.clipboard.dangerouslyPasteHTML(textTmp);
+      handleChangeText(textTmp);
+      setSign("");
+      setContractSign({});
+      quillRef?.current?.enable(false);
+      sendEmail();
+    }
+  }, [sign, contractSign]);
+  const addTable = () => {
+    debugger;
+    (quillRef.current as QuillNamespace)
+      .getModule("better-table")
+      .insertTable(3, 3);
+  };
+  const handleChangeText = useDebouncedCallback(
+    async (content: string) => {
+      let body = {
+        data: {
+          action: Action.UPDATE,
+          clientKey: clientKey,
+          userKey: userKey,
+          contract: { contractKey: contractKey, value: content },
+        },
+      };
+      await axios
+        .post(BASE_URL + ApiEntity.CONTRACT, body, {
+          headers: {
+            Accept: "application/vnd.api+json",
+            "Content-Type": "application/vnd.api+json",
+            "x-sendforsign-key": "re_api_key", //process.env.SENDFORSIGN_API_KEY,
+          },
+          responseType: "json",
+        })
+        .then((payload) => {
+          console.log("editor read", payload);
+        });
+    },
+    5000,
+    // The maximum time func is allowed to be delayed before it's invoked:
+    { maxWait: 5000 }
+  );
+  const sendEmail = async () => {
+    let body = {
+      clientKey: clientKey,
+      contractKey: contractKey,
+    };
+    await axios
+      .post(BASE_URL + ApiEntity.CONTRACT_EMAIL_SIGN, body, {
+        headers: {
+          Accept: "application/vnd.api+json",
+          "Content-Type": "application/vnd.api+json",
+          "x-sendforsign-key": "re_api_key", //process.env.SENDFORSIGN_API_KEY,
+        },
+        responseType: "json",
+      })
+      .then((payload) => {
+        console.log("editor read", payload);
+      });
+  };
+
+  return (
+    <Space direction="vertical" size={16} style={{ display: "flex" }}>
+      <Card loading={continueLoad}>
+        <Space direction="vertical" size={16} style={{ display: "flex" }}>
+          <Space direction="vertical" size={2}>
+            <Title level={4} style={{ margin: "0 0 0 0" }}>
+              Review your document, highlight text to see options
+            </Title>
+            <Text type="secondary">
+              The green text is where you may want to replace it with your own
+              text.
+            </Text>
+          </Space>
+          <div id="scroll-container">
+            <div id="contract-editor-container" />
+          </div>
+        </Space>
+      </Card>
+    </Space>
+  );
 };
--- src/components/template-editor/choose-template-type/choose-template-type.tsx
+++ src/components/template-editor/choose-template-type/choose-template-type.tsx
@@ -1,247 +1,244 @@
-import React, { useEffect, useState } from 'react';
-import { Space, Card, Typography, Button, Tag, Input } from 'antd';
-import { ContractType } from '../../../config/enum';
-import Segmented, { SegmentedLabeledOption } from 'antd/es/segmented';
-import { useTemplateEditorContext } from '../template-editor-context';
-import useSaveArrayBuffer from '../../../hooks/use-save-array-buffer';
-import { docx2html } from '../../../utils';
+import React, { useEffect, useState } from "react";
+import { Space, Card, Typography, Button, Tag, Input } from "antd";
+import { ContractType } from "../../../config/enum";
+import Segmented, { SegmentedLabeledOption } from "antd/es/segmented";
+import { useTemplateEditorContext } from "../template-editor-context";
+import useSaveArrayBuffer from "../../../hooks/use-save-array-buffer";
+import { docx2html } from "../../../utils";
 
 export const ChooseTemplateType = () => {
-	const {
-		setIsPdf,
-		templateName,
-		setTemplateName,
-		setTemplateType,
-		setTemplateValue,
-		setCreateTemplate,
-		pdfFileLoad,
-		setPdfFileLoad,
-		continueLoad,
-		setContinueLoad,
-	} = useTemplateEditorContext();
-	// if (!process.env.SENDFORSIGN_API_KEY) {
-	// 	console.log(
-	// 		'process.env.SENDFORSIGN_API_KEY',
-	// 		process.env.SENDFORSIGN_API_KEY
-	// 	);
-	// }
-	const [options, setOptions] = useState<SegmentedLabeledOption[]>([]);
-	const { setArrayBuffer } = useSaveArrayBuffer();
-	const [createDisable, setCreateDisable] = useState(true);
-	const [continueDisable, setContinueDisable] = useState(true);
-	const [fieldBlockVisible, setFieldBlockVisible] = useState(false);
-	const [loadSegmented, setLoadSegmented] = useState(false);
-	const [btnName, setBtnName] = useState('Create template');
-	const { Title, Text } = Typography;
+  const {
+    setIsPdf,
+    templateName,
+    setTemplateName,
+    setTemplateType,
+    setTemplateValue,
+    setCreateTemplate,
+    pdfFileLoad,
+    setPdfFileLoad,
+    continueLoad,
+    setContinueLoad,
+  } = useTemplateEditorContext();
+  // if (!process.env.SENDFORSIGN_API_KEY) {
+  // 	console.log(
+  // 		'process.env.SENDFORSIGN_API_KEY',
+  // 		process.env.SENDFORSIGN_API_KEY
+  // 	);
+  // }
+  const [options, setOptions] = useState<SegmentedLabeledOption[]>([]);
+  const { setArrayBuffer } = useSaveArrayBuffer();
+  const [createDisable, setCreateDisable] = useState(true);
+  const [continueDisable, setContinueDisable] = useState(true);
+  const [fieldBlockVisible, setFieldBlockVisible] = useState(false);
+  const [loadSegmented, setLoadSegmented] = useState(false);
+  const [btnName, setBtnName] = useState("Create template");
+  const { Title, Text } = Typography;
 
-	useEffect(() => {
-		setOptions([
-			{
-				label: (
-					<div
-						style={{
-							paddingTop: '8px',
-							width: 100,
-							whiteSpace: 'normal',
-							lineHeight: '20px',
-						}}
-					>
-						<Tag style={{ margin: '4px 0' }} color={'magenta'}>
-							File
-						</Tag>
-						<div style={{ padding: '4px 0' }}>Upload your DOCX file</div>
-					</div>
-				),
-				value: ContractType.DOCX,
-			},
-			{
-				label: (
-					<div
-						style={{
-							paddingTop: '8px',
-							width: 100,
-							whiteSpace: 'normal',
-							lineHeight: '20px',
-						}}
-					>
-						<Tag style={{ margin: '4px 0' }} color={'magenta'}>
-							File
-						</Tag>
-						<div style={{ padding: '4px 0' }}>Upload your PDF file</div>
-					</div>
-				),
-				value: ContractType.PDF,
-			},
-			{
-				label: (
-					<div
-						style={{
-							paddingTop: '8px',
-							width: 100,
-							whiteSpace: 'normal',
-							lineHeight: '20px',
-						}}
-					>
-						<Tag style={{ margin: '4px 0' }} color={'cyan'}>
-							Empty
-						</Tag>
-						<div style={{ padding: '4px 0' }}>Draft from scratch</div>
-					</div>
-				),
-				value: ContractType.EMPTY,
-			},
-		]);
-	}, []);
+  useEffect(() => {
+    setOptions([
+      {
+        label: (
+          <div
+            style={{
+              paddingTop: "8px",
+              width: 100,
+              whiteSpace: "normal",
+              lineHeight: "20px",
+            }}
+          >
+            <Tag style={{ margin: "4px 0" }} color={"magenta"}>
+              File
+            </Tag>
+            <div style={{ padding: "4px 0" }}>Upload your DOCX file</div>
+          </div>
+        ),
+        value: ContractType.DOCX,
+      },
+      {
+        label: (
+          <div
+            style={{
+              paddingTop: "8px",
+              width: 100,
+              whiteSpace: "normal",
+              lineHeight: "20px",
+            }}
+          >
+            <Tag style={{ margin: "4px 0" }} color={"magenta"}>
+              File
+            </Tag>
+            <div style={{ padding: "4px 0" }}>Upload your PDF file</div>
+          </div>
+        ),
+        value: ContractType.PDF,
+      },
+      {
+        label: (
+          <div
+            style={{
+              paddingTop: "8px",
+              width: 100,
+              whiteSpace: "normal",
+              lineHeight: "20px",
+            }}
+          >
+            <Tag style={{ margin: "4px 0" }} color={"cyan"}>
+              Empty
+            </Tag>
+            <div style={{ padding: "4px 0" }}>Draft from scratch</div>
+          </div>
+        ),
+        value: ContractType.EMPTY,
+      },
+    ]);
+  }, []);
 
-	const handleCreate = () => {
-		setFieldBlockVisible(true);
-		setCreateDisable(true);
-	};
+  const handleCreate = () => {
+    setFieldBlockVisible(true);
+    setCreateDisable(true);
+  };
 
-	const handleContinue = async () => {
-		setContinueLoad(true);
-		setCreateTemplate(true);
-	};
+  const handleContinue = async () => {
+    setContinueLoad(true);
+    setCreateTemplate(true);
+  };
 
-	const handleChange = (e: any) => {
-		switch (e.target.id) {
-			case 'TemplateName':
-				setTemplateName(e.target.value);
-				if (e.target.value) {
-					setContinueDisable(false);
-				} else {
-					setContinueDisable(true);
-				}
-				break;
-		}
-	};
+  const handleChange = (e: any) => {
+    switch (e.target.id) {
+      case "TemplateName":
+        setTemplateName(e.target.value);
+        if (e.target.value) {
+          setContinueDisable(false);
+        } else {
+          setContinueDisable(true);
+        }
+        break;
+    }
+  };
 
-	const handleChoose = async (e: any) => {
-		if (
-			e.toString() === ContractType.DOCX.toString() ||
-			e.toString() === ContractType.PDF.toString()
-		) {
-			setBtnName('Upload file');
-		} else {
-			setBtnName('Create template');
-		}
-		let input = null;
-		switch (e) {
-			case ContractType.DOCX:
-				input = document.createElement('input');
-				input.type = 'file';
-				input.accept =
-					'application/vnd.openxmlformats-officedocument.wordprocessingml.document';
+  const handleChoose = async (e: any) => {
+    if (
+      e.toString() === ContractType.DOCX.toString() ||
+      e.toString() === ContractType.PDF.toString()
+    ) {
+      setBtnName("Upload file");
+    } else {
+      setBtnName("Create template");
+    }
+    let input = null;
+    switch (e) {
+      case ContractType.DOCX:
+        input = document.createElement("input");
+        input.type = "file";
+        input.accept =
+          "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
 
-				input.onchange = (e: any) => {
-					// debugger;
-					let file = e.target.files[0];
-					let reader = new FileReader();
-					reader.readAsArrayBuffer(file);
-					reader.onload = (readerEvent) => {
-						if (readerEvent) {
-							docx2html(
-								readerEvent?.target?.result as ArrayBuffer,
-								(payload: any) => {
-									debugger;
-									setTemplateValue(payload);
-									setTemplateType(ContractType.DOCX.toString());
-									setCreateDisable(false);
-								}
-							);
-						}
-					};
-				};
+        input.onchange = (e: any) => {
+          // debugger;
+          let file = e.target.files[0];
+          let reader = new FileReader();
+          reader.readAsArrayBuffer(file);
+          reader.onload = (readerEvent) => {
+            if (readerEvent) {
+              docx2html(
+                readerEvent?.target?.result as ArrayBuffer,
+                (payload: any) => {
+                  debugger;
+                  setTemplateValue(payload);
+                  setTemplateType(ContractType.DOCX.toString());
+                  setCreateDisable(false);
+                }
+              );
+            }
+          };
+        };
 
-				input.click();
-				break;
-			case ContractType.PDF:
-				input = document.createElement('input');
-				input.type = 'file';
-				input.accept = 'application/pdf';
+        input.click();
+        break;
+      case ContractType.PDF:
+        input = document.createElement("input");
+        input.type = "file";
+        input.accept = "application/pdf";
 
-				input.onchange = (e: any) => {
-					let file = e.target.files[0];
-					const fileSize = Math.round(file.size / 1048576);
-					if (fileSize > 15) {
-						alert('File too big, please select a file less than 15mb');
-						return;
-					}
-					let reader = new FileReader();
-					reader.readAsArrayBuffer(file);
-					reader.onload = async (readerEvent) => {
-						debugger;
-						setTemplateType(ContractType.PDF.toString());
-						setIsPdf(true);
-						const arrayBuffer = readerEvent?.target?.result as ArrayBuffer;
-						await setArrayBuffer('pdfFileTemplate', arrayBuffer);
-						setCreateDisable(false);
-						setPdfFileLoad(pdfFileLoad + 1);
-					};
-				};
+        input.onchange = (e: any) => {
+          let file = e.target.files[0];
+          const fileSize = Math.round(file.size / 1048576);
+          if (fileSize > 15) {
+            alert("File too big, please select a file less than 15mb");
+            return;
+          }
+          let reader = new FileReader();
+          reader.readAsArrayBuffer(file);
+          reader.onload = async (readerEvent) => {
+            debugger;
+            setTemplateType(ContractType.PDF.toString());
+            setIsPdf(true);
+            const arrayBuffer = readerEvent?.target?.result as ArrayBuffer;
+            await setArrayBuffer("pdfFileTemplate", arrayBuffer);
+            setCreateDisable(false);
+            setPdfFileLoad(pdfFileLoad + 1);
+          };
+        };
 
-				input.click();
-				break;
-			case ContractType.EMPTY:
-				setTemplateType(ContractType.EMPTY.toString());
-				setCreateDisable(false);
-				break;
-			default:
-				break;
-		}
-	};
+        input.click();
+        break;
+      case ContractType.EMPTY:
+        setTemplateType(ContractType.EMPTY.toString());
+        setCreateDisable(false);
+        break;
+      default:
+        break;
+    }
+  };
 
-	return (
-		<Space direction='vertical' size={16} style={{ display: 'flex' }}>
-			<Card bordered={true} loading={loadSegmented}>
-				<Space direction='vertical' size={16} style={{ display: 'flex' }}>
-					<Space direction='vertical' size={2}>
-						<Title level={4} style={{ margin: '0' }}>
-							Draft from scratch or upload a file
-						</Title>
-						<Text type='secondary'>
-							This will speed up the drafting process.
-						</Text>
-					</Space>
-					<Segmented
-						options={options}
-						onChange={handleChoose}
-					/>
-					<Button
-						type='primary'
-						disabled={createDisable}
-						onClick={handleCreate}
-					>
-						{btnName}
-					</Button>
-				</Space>
-			</Card>
-			{fieldBlockVisible && (
-				<Card bordered={true}>
-					<Space direction='vertical' size={16} style={{ display: 'flex' }}>
-						<Space direction='vertical' size={2}>
-							<Title level={4} style={{ margin: '0' }}>
-								Fill additional information on the template
-							</Title>
-						</Space>
-						<Input
-							id='TemplateName'
-							placeholder='Enter your template name'
-							value={templateName}
-							onChange={handleChange}
-							// readOnly={!continueDisable}
-						/>
-						<Button
-							type='primary'
-							disabled={continueDisable}
-							onClick={handleContinue}
-							loading={continueLoad}
-						>
-							Continue
-						</Button>
-					</Space>
-				</Card>
-			)}
-		</Space>
-	);
+  return (
+    <Space direction="vertical" size={16} style={{ display: "flex" }}>
+      <Card loading={loadSegmented}>
+        <Space direction="vertical" size={16} style={{ display: "flex" }}>
+          <Space direction="vertical" size={2}>
+            <Title level={4} style={{ margin: "0" }}>
+              Draft from scratch or upload a file
+            </Title>
+            <Text type="secondary">
+              This will speed up the drafting process.
+            </Text>
+          </Space>
+          <Segmented options={options} onChange={handleChoose} />
+          <Button
+            type="primary"
+            disabled={createDisable}
+            onClick={handleCreate}
+          >
+            {btnName}
+          </Button>
+        </Space>
+      </Card>
+      {fieldBlockVisible && (
+        <Card bordered={true}>
+          <Space direction="vertical" size={16} style={{ display: "flex" }}>
+            <Space direction="vertical" size={2}>
+              <Title level={4} style={{ margin: "0" }}>
+                Fill additional information on the template
+              </Title>
+            </Space>
+            <Input
+              id="TemplateName"
+              placeholder="Enter your template name"
+              value={templateName}
+              onChange={handleChange}
+              // readOnly={!continueDisable}
+            />
+            <Button
+              type="primary"
+              disabled={continueDisable}
+              onClick={handleContinue}
+              loading={continueLoad}
+            >
+              Continue
+            </Button>
+          </Space>
+        </Card>
+      )}
+    </Space>
+  );
 };
