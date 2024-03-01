c507cf119b441beae5697ccaf91a2c56583b0bbb
cat-unicorn <111731558+cat-unicorn@users.noreply.github.com>
2024-03-01 12:59:40 +0300

    storybook

--- storybook-static/assets/preview-BkOW7CsI.js
+++
@@ -1,7 +0,0 @@
-import{_ as t}from"./iframe-BeTIuNQ-.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-WtKJM-Oo.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10,11]),import.meta.url);return new r}}};export{_ as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-WtKJM-Oo.js","./iframe-BeTIuNQ-.js","./index-BsHCltJw.js","./_commonjsHelpers-BosuxZz1.js","./react-16-Csl-nCkB.js","./index-BY6ttnmU.js","./_commonjs-dynamic-modules-TwCWkOBp.js","./index-Bw8VTzHM.js","./inheritsLoose-MVK87o0N.js","./_getPrototype-C_maDOEK.js","./isSymbol-DB5r9LRl.js","./index-DrFu-skq.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
